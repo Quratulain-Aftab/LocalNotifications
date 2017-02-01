2a5930a5ad20f2784ef6b9d414cedd73e5d09929
Oracular IS <Quratuain@Oraculars-MacBook-Pro.local>
2017-02-01 17:32:14 +0500

    commit

--- NotificationTemplate/ViewController.swift
+++ NotificationTemplate/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+let dataSource = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
+    
+{
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var dataPicker: UIPickerView!
+    @IBOutlet weak var reminderTextField: UITextField!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.segmentControl.selectedSegmentIndex=2
+        
+        self.dataPicker.delegate=self
+        self.dataPicker.dataSource=self
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +39,20 @@ class ViewController: UIViewController {
 
 }
 
+
+// DataSource
+func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    
+}
+func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    
+}
+
+// Delegate
+
+func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    
+}
+func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    
+}
